550466a4ee0d964ce27aea63fe8afaab41d4ece9
Aaron Ogle <atogle@gmail.com>
2011-05-11 13:26:16 -0700

    Simplify syncing.

--- scripts/sync_events.js
+++ scripts/sync_events.js
@@ -6,40 +6,9 @@ var ical = require('ical'),
 //Setup the DB connection
 var db = new (cradle.Connection)('127.0.0.1','5984').database('togather');
 
-
-//Deletes current events that are not also new events
-//We saved them previously, but they are no longer
-//in the feed.
-var deleteOldEvents = function(newEvents, currentEvents) {
-    var uid,
-        i,
-        toDelete = [];
-
-    for (uid in currentEvents) {
-        if (currentEvents.hasOwnProperty(uid)) {
-            
-            //if the current event is not in the list of new events,
-            //then mark it for deletion
-            if (!newEvents.uid) {
-                toDelete.push({
-                    id: currentEvents[uid]._id,
-                    rev: currentEvents[uid]._rev
-                });
-            }
-        }
-    }
-    
-    //Delete old records from the db
-    for (i=0; i<toDelete.length; i++) {
-        db.remove(toDelete[i].id, toDelete[i].rev, function (err, res) {
-            console.log('deleting ', res);
-        });
-    }
-};
-
 //Saves new events to the db. This will create events if
 //they don't exist or replace them if they do.
-var saveNewEvents = function(newEvents, url, callback) {
+var saveEvents = function(newEvents, url, callback) {
     var uid,
         hash,
         docs = [];
@@ -73,30 +42,6 @@ var saveNewEvents = function(newEvents, url, callback) {
     });    
 };
 
-//Sync events with the db
-var syncEvents = function(newEvents, currentEvents, url) {
-    //No old events to delete because we recreate the db
-    saveNewEvents(newEvents, url);
-};
-
-//Get the events we've already stored for this url
-var getCurrentEvents = function(url, callback) {
-    db.view('events/origin_url', { key: url },
-        function (err, docs) {
-            var i, currentEvents = {};
-            
-            if (err) {
-                console.log(err);
-            } else {
-                for (i=0; i<docs.length; i++) {
-                    currentEvents[docs[i].value.uid] = docs[i].value;
-                }
-            }
-            
-            callback(currentEvents, url);
-        }
-    );
-};
 
 //Parse an ical ics file
 var parseIcsFile = function(url, callback) {
@@ -113,28 +58,25 @@ var parseIcsFile = function(url, callback) {
 };
 
 db.destroy(function() {
-    db.create();
-    
-    db.save('_design/events', {
-        origin_url: {
-            map: function (doc) {
-                emit(doc.origin_url, doc);
+    db.create(function() {
+        db.save('_design/events', {
+            origin_url: {
+                map: function (doc) {
+                    emit(doc.origin_url, doc);
+                }
             }
-        }
-    });
-    
-    //Each line of this file is an ics url
-    fs.readFile('./ics_urls.txt', function (err, data) {
-        var lines = data.toString().split('\n'),
-            i;
+        });
 
+        //Each line of this file is an ics url
+        fs.readFile('./ics_urls.txt', function (err, data) {
+            var lines = data.toString().split('\n'),
+                i;
 
-        for (i=0; i<lines.length; i++) {
-            parseIcsFile(lines[i], function(newEvents, url) {
-                getCurrentEvents(url, function(currentEvents, url2) {
-                    syncEvents(newEvents, currentEvents, url2);
+            for (i=0; i<lines.length; i++) {
+                parseIcsFile(lines[i], function(newEvents, url) {
+                    saveEvents(newEvents, url);
                 });
-            });
-        }
-    });        
+            }
+        });        
+    });
 });
