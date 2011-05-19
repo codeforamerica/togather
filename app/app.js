1ec1791e68a365a1a9e3159746f7894520fc1dda
Aaron Ogle <atogle@gmail.com>
2011-05-18 22:52:59 -0700

    Start adding support for adding new events.

--- app/app.js
+++ app/app.js
@@ -1,46 +1,46 @@
 var express = require('express'),
-    events = require('./cal-events');
+  events = require('./cal-events');
 
 var app = module.exports = express.createServer();
 
 // Configuration
 app.configure(function(){
-    app.set('views', __dirname + '/views');
-    app.set('view engine', 'ejs');
-    app.use(express.bodyParser());
-    app.use(express.methodOverride());
-    app.use(app.router);
-    app.use(express['static'](__dirname + '/public'));
+  app.set('views', __dirname + '/public/views');
+  app.set('view engine', 'ejs');
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express['static'](__dirname + '/public'));
 });
 
 app.configure('development', function(){
-    app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-    app.use(express.errorHandler()); 
+  app.use(express.errorHandler()); 
 });
 
 // Routes
 app.get('/', function(req, res) {
-    events.get(function(evts){
-        res.render('event-microdata', {
-            'title': 'Events Dashboard (hCal)',
-            'events': evts
-        });
+  events.get(function(evts){
+    res.render('event-microdata', {
+      'title': 'Events Dashboard',
+      'events': evts
     });
+  });
 });
 
-app.get('/add/:url', function(req, res) {
-    events.get(function(evts){
-        res.send('url: ' + req.params.url);
-        events.addUrl(req.params.url);
-    });
+app.get('/add', function(req, res) {
+  events.addUrl(decodeURIComponent(req.query.url), function(evts) {
+    res.contentType('application/json');
+    res.send(evts);
+  });
 });
 
 
 // Only listen on $ node app.js
 if (!module.parent) {
-    app.listen(8080);
-    console.log("Express server listening on port %d", app.address().port);
+  app.listen(8080);
+  console.log("Express server listening on port %d", app.address().port);
 }
--- app/public/togather/tg-add.js
+++ app/public/togather/tg-add.js
@@ -1,3 +1,82 @@
-function(options){
+(function(){
+  var self = {},
+      options = {
+        openDialogButtonId: 'cal-dialog-open-button',
+        closeDialogButtonId: 'cal-dialog-close-button',
+        addUrlButtonId: 'cal-add-button',
+        addUrlInputId: 'cal-add-input',
+        overlayId: 'cal-overlay',
+        boxId: 'cal-box'
+      },
+      sb,
+      eventEjs = new EJS({url: 'views/event-microdata.ejs'}),
+      $box = $('#' + options.boxId);
+    
+  var refresh = function(events) {
+    $('#cal-add-preview').html(eventEjs.render( {events: [events[0]]} ));
+  };
   
-};
+  self.close = function() {
+    console.log('close!');
+    sb.close();
+  };
+  
+  self.open = function() {
+    sb.open();
+  };
+  
+  var bindEvents = function() {
+    
+    $('#' + options.closeDialogButtonId).click(function(){
+      self.close();
+    });
+    
+    //Open the dialog
+    $('#' + options.openDialogButtonId).click(function() {
+      self.open();
+    });
+    
+    $('#' + options.addUrlButtonId).click(function() {
+      var url = $('#' + options.addUrlInputId).val();
+      
+      //TODO: make a regex check
+      if (url) {
+        $.ajax({
+          url: 'add',
+          type: 'GET',
+          dataType: 'json',
+          data: { url: encodeURIComponent(url) },
+          success: function(data, textStatus, jqXHR) {
+            console.log(data);
+            
+            refresh(data);
+            
+            //self.close();
+          },
+          error: function(jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown);
+          }
+        });
+      }
+    });
+  };
+  
+  var init = function() {
+    $('<div id="cal-overlay" class="skinny-overlay"></div>' + 
+      '<div id="cal-box" class="skinny-box"><header><h2>Enter the URL for your event:</h2></header>' + 
+      '<section><input type="text" id="cal-add-input" value="" /><button id="cal-add-button">Add</button></section>' + 
+      '<section id="cal-add-preview"></section>' + 
+      '<footer><button id="'+options.closeDialogButtonId+'">close!</button><footer></div>').appendTo('body');
+    
+    sb = skinnybox({
+      overlay: options.overlayId, 
+      box: options.boxId
+    });
+      
+    bindEvents();
+  };
+  
+  init();
+  
+  return self;
+})();
